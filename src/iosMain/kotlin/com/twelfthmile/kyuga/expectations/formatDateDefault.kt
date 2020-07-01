59baac9010bd13b32d7a777965275bfad3cbac3f
Vishnu Satis <vishnu.satis@truecaller.com>
2020-07-01 15:43:35 +0530

    Removing few log statements

--- src/iosMain/kotlin/com/twelfthmile/kyuga/expectations/formatDateDefault.kt
+++ src/iosMain/kotlin/com/twelfthmile/kyuga/expectations/formatDateDefault.kt
@@ -6,21 +6,17 @@ import platform.Foundation.NSLocale
 private const val DATE_TIME_FORMAT_STR = "yyyy-MM-dd HH:mm:ss"
 
 actual fun formatDateDefault(multDate: MultDate): String {
-    print("[twDebug] Input date: " + multDate.date)
     val dateFormatter = NSDateFormatter()
     dateFormatter.setDateFormat(DATE_TIME_FORMAT_STR)
     val dateString = dateFormatter.stringFromDate(multDate.date)
-    print("[twDebug] Output date: $dateString")
     return dateString
 }
 
 actual fun formatDateByFormat(date: String, format: String): MultDate {
-    print("[twDebug] Input date: $date")
     val dateFormatter = NSDateFormatter()
     dateFormatter.setDateFormat(format)
     val dt = MultDate()
     dt.date = dateFormatter.dateFromString(date)!!
-    print("[twDebug] Output date: $dt")
     return dt
 }
 
@@ -28,12 +24,10 @@ actual fun formatForEnglishLocale(
     date: String,
     format: String
 ): MultDate {
-    print("[twDebug] Input date: $date")
     val dateFormatter = NSDateFormatter()
     dateFormatter.setDateFormat(format)
     dateFormatter.locale = NSLocale("en_GB")
     val dt = MultDate()
     dt.date = dateFormatter.dateFromString(date)!!
-    print("[twDebug] Output date: $dt")
     return dt
 }
